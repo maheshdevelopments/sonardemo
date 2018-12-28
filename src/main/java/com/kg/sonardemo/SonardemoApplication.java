df161dbf0ddb83ee8499655d2394050d71c664f8
Baraneetharan Ramasamy <baraneetharan.r@kgisl.com>
2018-12-28 16:11:08 +0530

    first commit

---
+++ src/main/java/com/kg/sonardemo/SonardemoApplication.java
@@ -0,0 +1,14 @@
+package com.kg.sonardemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonardemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonardemoApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/kg/sonardemo/SonardemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kg.sonardemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonardemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
